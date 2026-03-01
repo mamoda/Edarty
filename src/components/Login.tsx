8e1103ea8a55c19e9d45200f0b96d7255eec5691
Mahmoud Attia <51999205+mamoda@users.noreply.github.com>
2026-03-01 21:32:56 +0200

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -55,7 +55,6 @@ export default function Login() {
                 alt="شعار التطبيق"
                 className="h-12 w-auto"
               />
-            <h1 className="text-3xl font-bold text-gray-900 mb-2">إدارتــي</h1>
             <p className="text-gray-600 text-center">بيانات أكثر وتقارير أدق وسهولة استخدام</p>
           </div>
 
